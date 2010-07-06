0f9a465efcb976a369706092fc0b829d45f258a6
Leonardo Xavier <leo@skipper.lan>
2010-07-06 10:42:20 +0100

    Deactivated buggy preloader

--- assets/js/preload.js
+++ assets/js/preload.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
-	
+/*	
   // http://www.filamentgroup.com/lab/update_automatically_preload_images_from_css_with_jquery/
   $('#page').css({opacity: 0});
   $.preloadCssImages();
-	
+	*/
 });
 
 // fade in the wrapper on .load, triggered by the css preloader plugin
 $(window).load(function () {
-  $('#page').animate({opacity: 1}, 1000);
+
+ // $('#page').animate({opacity: 1}, 1000);
+
 });
