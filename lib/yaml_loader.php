f2fbe32d134be18bfd6d325e3e82485a721ee960
Luis Abreu <lmjabreu@gmail.com>
2011-04-15 14:26:24 +0100

    fixes to how the ftm_config file is loaded

--- lib/yaml_loader.php
+++ lib/yaml_loader.php
@@ -2,14 +2,18 @@
 
 // include "A simple YAML loader/dumper" named spyc
 require_once "spyc.php";
+// set the filepath
+$ftm_config_path = dirname(__FILE__) . '/../ftm_config.yml';
+// load the ftm_config.yml
+$ftm_config = fread(fopen($ftm_config_path, 'r'), filesize($ftm_config_path)) or false;
 
 // create the config file if it doesn't exist
-if (!file_exists('ftm_config.yml')) {
-  touch('ftm_config.yml');
+if (!$ftm_config) {
+  touch($ftm_config_path);
 }
 
 // read the configuration file
-$config = Spyc::YAMLLoad('ftm_config.yml');
+$config = Spyc::YAMLLoad($ftm_config);
 
 // return the config array
 return($config);
